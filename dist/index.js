eceff0fad2ccb9f5191c9736b99406c803ea098d
Gels04 <gelcira2004@outlook.com>
2023-06-02 10:22:34 +0100

    not really a ToDo List, but actually a counter using TS

---
+++ dist/index.js
@@ -0,0 +1,55 @@
+"use strict";
+const number = document.querySelector("#number");
+const downButton = document.querySelector("#downButton");
+const resetButton = document.querySelector("#resetButton");
+const upButton = document.querySelector("#upButton");
+let count = 0;
+console.log(resetButton);
+downButton === null || downButton === void 0 ? void 0 : downButton.addEventListener("click", () => {
+    count -= 1;
+    if ((number === null || number === void 0 ? void 0 : number.textContent) == null || number.textContent == "") { //I always had to eliminate the possibility of number being null || undefined
+        return;
+    }
+    number.textContent = count.toString();
+    colourCheck();
+    console.log(number);
+});
+resetButton === null || resetButton === void 0 ? void 0 : resetButton.addEventListener("click", () => {
+    count = 0;
+    if ((number === null || number === void 0 ? void 0 : number.textContent) == null || number.textContent == "") {
+        return;
+    }
+    number.textContent = count.toString();
+    // console.log("reset button clicked!!")
+    colourCheck();
+});
+upButton === null || upButton === void 0 ? void 0 : upButton.addEventListener("click", (e) => {
+    count += 1;
+    if ((number === null || number === void 0 ? void 0 : number.textContent) == null || number.textContent == "")
+        return;
+    number.textContent = count.toString();
+    // console.log("up button clicked!!")
+    colourCheck();
+});
+function colourCheck() {
+    if ((number === null || number === void 0 ? void 0 : number.textContent) == null || (number === null || number === void 0 ? void 0 : number.textContent) == "") {
+        return;
+    }
+    //it was a challenge to convert type Element to a number without throwing any errors
+    let realNumber = +number.textContent;
+    if (number.textContent == "0") {
+        number.className = "black";
+    }
+    else if (realNumber < 0) {
+        number.className = "blue";
+    }
+    else if (realNumber > 0) {
+        number.className = "red";
+    }
+}
+//WHAT I LEARNT
+//typeScript is VERY strict! i was sometimes annoyed at TS 
+//because I knew i'd be able to get away with writing the same code in JS
+//I had to remind myself that TS helps minimise errors and bugs.
+//I'm getting used to TS syntax, although I need a little more pracitce to become comfortable with working a strict program,
+//and still produce great, functioning code
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,67 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const number = document.querySelector("#number")
+const downButton = document.querySelector("#downButton")
+const resetButton = document.querySelector("#resetButton")
+const upButton = document.querySelector("#upButton")
 
-import confetti from 'canvas-confetti';
+let count = 0;
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+console.log(resetButton)
+
+downButton?.addEventListener("click", () => {
+  count -= 1;
+  if(number?.textContent == null || number.textContent == "") { //I always had to eliminate the possibility of number being null || undefined
+    return 
+  }
+  number.textContent = count.toString()
+
+  colourCheck()
+  console.log(number)
+})
+
+resetButton?.addEventListener("click", () => {
+  count = 0;
+  if(number?.textContent == null || number.textContent == "") {
+    return 
+  }
+  number.textContent = count.toString()
+  // console.log("reset button clicked!!")
+  colourCheck()
+})
+
+upButton?.addEventListener("click", (e) => {
+
+  count += 1;
+
+  if(number?.textContent == null || number.textContent == "") return
+
+  number.textContent = count.toString()
+  // console.log("up button clicked!!")
+  colourCheck()
+})
+
+function colourCheck(): void {
+  if(number?.textContent == null || number?.textContent == ""){
+    return
+  } 
+
+
+  //it was a challenge to convert type Element to a number without throwing any errors
+  
+  let realNumber = +number.textContent
+  
+
+  if(number.textContent == "0") {
+    number.className = "black"
+  } else if (realNumber < 0) {
+    number.className = "blue"
+  } else if(realNumber > 0) {
+    number.className = "red"
+  }
+}
+
+//WHAT I LEARNT
+//typeScript is VERY strict! i was sometimes annoyed at TS 
+//because I knew i'd be able to get away with writing the same code in JS
+//I had to remind myself that TS helps minimise errors and bugs.
+//I'm getting used to TS syntax, although I need a little more pracitce to become comfortable with working a strict program,
+//and still produce great, functioning code
